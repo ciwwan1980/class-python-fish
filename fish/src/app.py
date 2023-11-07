6f50eca57ce7cab9be6e70bec74619d82b4ef8dd
ciwwan1980 <ajilqado@gmail.com>
2023-11-07 21:11:28 +0100

    add start-metho-to-function-call-new-metho

--- fish/src/app.py
+++ fish/src/app.py
@@ -1,5 +1,5 @@
 class AquariumApp:
-    
+
     PRIVATE_SWIM_COUNT = 0
 
     def __init__(self, fish_count, eye_color, skin_color):
@@ -9,12 +9,27 @@ class AquariumApp:
         self.protected_DEAD_FISH = 0
 
 
+    def start(self):
+        if self.fish_count == 0:
+            print("There are no alive fish left.")
+            return
+        self.PRIVATE_SWIM_COUNT += 1
+
+        print(
+            f"{self.fish_count} fish are swimming. Their eyes are {self.eye_color} and their skin is {self.skin_color}."
+        )
+        print(
+            f"There are {self.protected_DEAD_FISH} dead fish with them in the aquarium."
+        )
+        print(f"The fish have now been swimming {self.PRIVATE_SWIM_COUNT} times.")
+
 # Initialize an instance of the class and print some attributes
 my_aquarium = AquariumApp(5, "blue", "red")
 
-Fish_count= my_aquarium.fish_count
-print(f"Fish_count {Fish_count}")
-# or to make it shorter, we call the instance with its attribute
+#Fish_count= my_aquarium.fish_count
+# print(f"Fish_count {Fish_count}")
+# # or to make it shorter, we call the instance with its attribute
 
-print(f"Eye_color: {my_aquarium.eye_color}")
-print(f"Skin color: {my_aquarium.skin_color}")
+# print(f"Eye_color: {my_aquarium.eye_color}")
+# print(f"Skin color: {my_aquarium.skin_color}")
+my_aquarium.start()
